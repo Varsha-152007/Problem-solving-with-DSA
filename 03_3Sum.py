b2f7a28f0e9af037432d429413763734f1f18e62
Varsha-152007 <varsharamesh207@gmail.com>
2026-08-21 23:08:38 +0530

    Solution for 3Sum problem

---
+++ 03_3Sum.py
@@ -0,0 +1,46 @@
+# Problem: 3Sum
+# Problem Link: https://leetcode.com/problems/3sum/description/
+# Date: 21st Aug 2026
+# Time taken to solve: 45 mins
+
+#solution
+<
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        result = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+
+                elif total < 0:
+                    left += 1
+
+                else:
+                    right -= 1
+
+        return result
+      >
+
+#Notes:
+#Used 2 pointers approach
